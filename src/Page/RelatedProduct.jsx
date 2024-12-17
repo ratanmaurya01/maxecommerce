fe1925138d52cb52f1a081e20d2f3101f16d1a0e
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-17 19:33:35 +0530

    add page footer , place order , payment type , add extra item in cart page

---
+++ src/Page/RelatedProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function RelatedProduct() {
+  return (
+    <div>
+        <div>
+              Related Products 
+        </div>
+      
+    </div>
+  )
+}
