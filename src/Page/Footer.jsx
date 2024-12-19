8591d34c54231d9c86b6319c98985719eff5da47
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-19 09:04:18 +0530

    added some extra feature

--- src/Page/Footer.jsx
+++ src/Page/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <footer className="mt-10 bg-white rounded-lg shadow  dark:bg-gray-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" className="hover:underline">RSTAR™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="#" className="hover:underline">Logo™</a>. All Rights Reserved.
                     </span>
                     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                         <li>
