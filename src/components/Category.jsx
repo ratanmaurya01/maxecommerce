b2b306a1e5fbbc8998a9c0cbae2182a7c602092f
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-23 21:35:51 +0530

    added some extra feature

---
+++ src/components/Category.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Category() {
+    return (
+        <>
+            <div>
+                <div>
+                    <div>
+                       <h6>Category Wise </h6>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
---
+++ src/components/Subcategory.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Subcategory() {
+  return (
+    <>
+      
+      <div>
+        <div>
+            <h6>Sub-Category product</h6>
+        </div>
+      </div>
+    
+    </>
+  )
+}
