c31b16d180323c3ce0aebf6dc624e65b86db3002
ratanmaurya01 <ratn.maxwell@gmail.com>
2024-12-24 09:57:29 +0530

    added some extra feature

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function History() {
-  return (
-    <div>
-      <div>
-         
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <div>
+                    <h6>History</h6>
+                </div>
+                <div>
+                       
+                </div>
+
+            </div>
+        </div>
+    )
 }
---
+++ src/components/TrendindProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TrendindProduct() {
+  return (
+    <div>
+        <div>
+            <div>
+                   <h6>Upcomming product </h6>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
